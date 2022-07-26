c9fd3483926b9de1e8f61dbcfe0bbf1cde9353b4
Josue Huancapaza Cordova <josuehcordova+cursos@gmail.com>
2022-07-26 18:49:05 -0500

    Clase 8: Colecciones de assets
    minificando assets

--- helloworld/app/controllers/IndexController.php
+++ helloworld/app/controllers/IndexController.php
@@ -1,13 +1,26 @@
 <?php
 
+use Phalcon\Assets\Filters\Cssmin;
+use Phalcon\Assets\Filters\Jsmin;
+
 class IndexController extends ControllerBase
 {
 
     public function indexAction()
     {
-        $this->assets->addCss("css/app.css");
-        $this->assets->addJs("js/app.js");
-        
+        $this->assets
+             ->collection("app.min.css")
+             ->setTargetPath("css/app.min.css")
+             ->setTargetUri("css/app.min.css")
+             ->addCss("css/app.css")
+             ->addFilter(new Cssmin());
+
+        $this->assets->collection("app.min.js")
+             ->setTargetPath("js/app.min.js")
+             ->setTargetUri("js/app.min.js")
+             ->addJs("js/app.js")
+             ->addFilter(new Jsmin());
+
         $this->view->msg = "Nuevo mensaje";
 
         $this->view->setVars([
